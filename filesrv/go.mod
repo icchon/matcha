5c88165864d686fb7bbc04f321d8f072c9dbce95
icchon <icchon1001@gmail.com>
2025-11-22 20:24:50 +0900

    add micro service: filesrv

---
+++ filesrv/go.mod
@@ -0,0 +1,8 @@
+module github.com/icchon/matcha/filesrv
+
+go 1.25.4
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/joho/godotenv v1.5.1
+)
---
+++ filesrv/internal/server/server.go
@@ -0,0 +1,83 @@
+package server
+
+import(
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+)
+
+
+type ServerConfig struct{
+	ServerAddress string
+	UploadDir string
+	BaseUrl string
+}
+
+type Server struct{
+	router *chi.Mux
+	config     *ServerConfig
+	httpServer *http.Server
+}
+
+func NewServer(config *ServerConfig) *Server{
+
+	if _, err := os.Stat(config.UploadDir); os.IsNotExist(err) {
+		if err := os.Mkdir(config.UploadDir, 0755); err != nil {
+			fmt.Printf("Failed to create uploads directory: %v\n", err)
+			return nil
+		}
+	}
+
+	r := chi.NewRouter()
+
+	server := &Server{
+		router: r,
+		config: config,
+	}
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	h := NewHandler(config.UploadDir, config.BaseUrl)
+	r.Post("/upload", h.UploadImageHandler)
+	
+	fileServer(r, "/images", http.Dir(config.UploadDir))
+	return server
+}
+
+
+func (s *Server) Start() error {
+	s.httpServer = &http.Server{
+		Addr:         s.config.ServerAddress,
+		Handler:      s.router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	log.Printf("Starting HTTP server on %s", s.config.ServerAddress)
+	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("could not listen on %s: %v", s.config.ServerAddress, err)
+	}
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	log.Println("Shutting down server gracefully...")
+	return s.httpServer.Shutdown(ctx)
+}
+
+
+func fileServer(r chi.Router, path string, root http.FileSystem) {
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", http.StatusMovedPermanently).ServeHTTP)
+		path += "/"
+	}
+	r.Handle(path+"*", http.StripPrefix(path, http.FileServer(root)))
+}
